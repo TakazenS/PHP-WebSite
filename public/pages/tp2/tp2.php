70d3ad83b5c5823eb2a433014ab4ee1904577afa
Moine Loris <moine.loris420@gmail.com>
2025-05-05 12:58:25 +0200

    Changes On TP 2

--- public/pages/tp2/tp2.php
+++ public/pages/tp2/tp2.php
@@ -18,8 +18,29 @@
                 </div>
             </span>
             <section class="main-section" id="main-section">
-                <div class="container-txt-main-section">
-                    <p class="txt-main-section">Vous retrouverez ici les différents exercices de chaques tp !</p>
+                <div class="memory">
+                    <section class="container-memory">
+                            <div class="card" ><img src="/web/public/images/memory/banane.png" alt="fruit1"></div>
+                            <div class="card" ><img src="/web/public/images/memory/banane.png" alt="fruit2"></div>
+                            <div class="card" ><img src="/web/public/images/memory/cerise.png" alt="fruit3"></div>
+                            <div class="card" ><img src="/web/public/images/memory/cerise.png" alt="fruit4"></div>
+                            <div class="card" ><img src="/web/public/images/memory/coconut.png" alt="fruit5"></div>
+                            <div class="card" ><img src="/web/public/images/memory/coconut.png" alt="fruit6"></div>
+                            <div class="card" ><img src="/web/public/images/memory/fraise.png" alt="fruit7"></div>
+                            <div class="card" ><img src="/web/public/images/memory/fraise.png" alt="fruit8"></div>
+                            <div class="card" ><img src="/web/public/images/memory/kiwi.png" alt="fruit9"></div>
+                            <div class="card" ><img src="/web/public/images/memory/kiwi.png" alt="fruit10"></div>
+                            <div class="card" ><img src="/web/public/images/memory/orange.png" alt="fruit11"></div>
+                            <div class="card" ><img src="/web/public/images/memory/orange.png" alt="fruit12"></div>
+                            <div class="card" ><img src="/web/public/images/memory/pasteque.png" alt="fruit13"></div>
+                            <div class="card" ><img src="/web/public/images/memory/pasteque.png" alt="fruit14"></div>
+                            <div class="card" ><img src="/web/public/images/memory/poire.png" alt="fruit15"></div>
+                            <div class="card" ><img src="/web/public/images/memory/poire.png" alt="fruit16"></div>
+                            <div class="card" ><img src="/web/public/images/memory/pomme.png" alt="fruit17"></div>
+                            <div class="card" ><img src="/web/public/images/memory/pomme.png" alt="fruit18"></div>
+                            <div class="card" ><img src="/web/public/images/memory/raisin.png" alt="fruit19"></div>
+                            <div class="card" ><img src="/web/public/images/memory/raisin.png" alt="fruit20"></div>
+                    </section>
                 </div>
             </section>
         </div>
